394a2ad73d995a080ef0fa72aeb89b384169bad2
Cahree <cmyrick1@umbc.edu>
2026-05-23 11:16:07 -0400

    rebuilt game.py after reviewing the solution

--- guessing_game/game.py
+++ guessing_game/game.py
@@ -1,38 +1,45 @@
-
 def main():
-    guess_range = input("Choose Range: ")
+  
+    guess_range = input("Choose range (ex: 1 100, -3 2000): ")
 
     range_split = guess_range.split()
-    left = int(range_split[0])
-    right = int(range_split[1])
+    left = range_split[0]
+    right = range_split[1]
 
-    guess = (left + right) // 2
+    try:
+    # must be a valid integer
+        left = int(left)
+        right = int(right)
 
-    while (True):
-        if guess_range == "quit":
-            return False
-        
-        print(f"guess: {guess}")
-        feedback = input("is this is? ")
-        
+    except ValueError:
+        print("Input not an integer.")
+
+
+    if left > right:
+        print("Left must be less than right")
+
+    while left <= right:
+        guess = (left + right) // 2
+        print(f"Guess: {guess}")
+
+        feedback = input("Too high, too low, or found: ").lower().strip()
+
         if feedback == "low":
             left = guess + 1
-            guess = (left + right ) // 2
         
         elif feedback == "high":
             right = guess - 1
-            guess = (left + right) // 2
-        
+
         elif feedback == "found":
-            print("Great! Thanks for playing.")
-            return False
-    
-        elif feedback == "quit" :
-            return False
+            print("Great. Thanks for playing!")
+            return
+
+        elif feedback == "quit":
+            return
         
         else:
-            print("Enter somthing valid: (ex: low, high, found)")
+            print("Enter a valid option (ex: low, high, found, quit).")
 
+    
 main()
-
         
