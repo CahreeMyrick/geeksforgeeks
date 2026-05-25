643ec682e314aebc09e65970f7163cf9332a259d
Cahree <cmyrick1@umbc.edu>
2026-05-25 17:59:42 -0400

    [rebuild 2] problems with input validation

--- guessing_game/game.py
+++ guessing_game/game.py
@@ -1,45 +1,64 @@
-def main():
-  
-    guess_range = input("Choose range (ex: 1 100, -3 2000): ")
 
-    range_split = guess_range.split()
-    left = range_split[0]
-    right = range_split[1]
+def validate_bounds(interval):
+  while True:
+    interval_split = interval.split()
+    left_bound = interval_split[0]
+    right_bound = interval_split[1]
+
+    if len(interval_split) > 2:
+        interval = input("Choose valid interval: ")
 
     try:
-    # must be a valid integer
-        left = int(left)
-        right = int(right)
+        left_bound = int(left_bound)
+        right_bound = int(right_bound)
 
     except ValueError:
-        print("Input not an integer.")
-
-
-    if left > right:
-        print("Left must be less than right")
-
-    while left <= right:
-        guess = (left + right) // 2
-        print(f"Guess: {guess}")
-
-        feedback = input("Too high, too low, or found: ").lower().strip()
-
-        if feedback == "low":
-            left = guess + 1
-        
-        elif feedback == "high":
-            right = guess - 1
-
-        elif feedback == "found":
-            print("Great. Thanks for playing!")
-            return
-
-        elif feedback == "quit":
-            return
-        
-        else:
-            print("Enter a valid option (ex: low, high, found, quit).")
-
+        interval = input("Choose valid integer values: ")
     
+    else:
+       break
+
+    if left_bound > right_bound:
+       interval = input("Left bound must be less than right bound: ")
+    
+    else:
+       break
+
+       
+
+  return left_bound, right_bound
+    
+def validate_feedback(feedback):
+    options = {"low", "high", "found", "quit"}
+    while True:
+        if feedback not in options:
+            feedback = input("Give valid feedback (ex: low, high, found): ")
+        else:
+           break
+    return feedback
+
+def main():
+  # get interval from user
+  interval = input("Choose interval (ex: 1 100, -5 1000): ")
+
+  left, right = validate_bounds(interval)
+
+  guess = (left + right) // 2
+
+  while True:
+    print(f"Guess: {guess}")
+    feedback = input("Feedback: ")
+    feedback = validate_feedback(feedback)
+
+    if feedback == "low":
+      left = guess + 1
+    elif feedback == "high":
+      right = guess - 1
+    elif feedback == "quit":
+       return
+    else:
+       print("Great. Thanks for playing.")
+       return
+    guess = (left + right) // 2
+
 main()
-        
